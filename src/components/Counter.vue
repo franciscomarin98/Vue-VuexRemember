91896c7313bf2f9484907b1b6abb01f84beae2eb
franciscomarin98 <francmarinc@gmail.com>
2021-09-24 13:43:18 -0500

    :sparkles: Recapitulando lo que son las mutaciones

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -2,6 +2,12 @@
     <h1>Counter - Vuex</h1>
     <h2>Direct access: {{ $store.state.counter }}</h2>
     <h2>Computed: {{ counter }}</h2>
+
+    <button @click="increment">+1</button>
+    <button @click="incrementByArgument">+5</button>
+    <button @click="decrement">-1</button>
+
+
     <h2>mapState: {{ counter }}</h2>
     <h2>lastMutation: {{ lastMutation }}</h2>
 </template>
@@ -20,6 +26,17 @@ export default {
         //     counter: state => state.counter,
         //     lastMutation: state => state.lastMutation
         // }]
+    },
+    methods: {
+        increment() {
+            this.$store.commit('INCREMENT')
+        },
+        incrementByArgument() {
+            this.$store.commit('INCREMENTBYARGUMENT', 5)
+        },
+        decrement() {
+            this.$store.commit('DECREMENT')
+        }
     },
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,25 @@
-import { createStore } from 'vuex'
+import {createStore} from 'vuex'
 
 // Create a new store instance.
 const store = createStore({
-    state () {
+    state() {
         return {
             counter: 0,
             lastMutation: 'none'
         }
     },
     mutations: {
-        increment (state) {
-            state.count++
+        INCREMENT(state) {
+            state.counter++
+            state.lastMutation = 'INCREMENT'
+        },
+        INCREMENTBYARGUMENT(state, value) {
+            state.counter += value
+            state.lastMutation = 'INCREMENT-BY-ARGUMENT'
+        },
+        DECREMENT(state) {
+            state.counter--
+            state.lastMutation = 'DECREMENT'
         }
     }
 })
